e899b53a0d79fa0eebdff406ff5475835f1fe514
hari-elangeswaran-au16 <ehvenga@gmail.com>
2021-06-07 06:32:08 +0530

    review id error

--- seed.js
+++ seed.js
@@ -1,7 +1,11 @@
 const UserModel = require("./models/User")
+const BookModel = require("./models/Book")
+const ReviewModel = require("./models/Review")
 
 async function seedDB(){
     await UserModel.deleteMany({})
+    await BookModel.deleteMany({})
+    await ReviewModel.deleteMany({})
 }
 
 
